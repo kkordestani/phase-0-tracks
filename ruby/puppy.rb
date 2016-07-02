3a48d534e11d08fb9e06089d9115d622479a5647
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-07-01 19:44:46 -0700

    Class/Puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#SPECIES-------------------------
+#Dog 
+#
+#CHARACTERISTICS-----------------
+#Legs/Paws: 4 
+#Tail: yes 
+#Name: varies
+#Fur color: varies 
+#Sniffing nose: yes
+#Cute: yes
+#
+#BEHAVIOR------------------------
+#Loyal
+#Plays fetch sometimes 
+#Chases after squirles 
+#Sleeps on your lap 
